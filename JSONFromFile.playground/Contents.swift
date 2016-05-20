79e892195679977c1f6130d0e0109954e889369d
Will <student@MacBook-Pro-0690-Graham.local>
2016-05-20 09:53:39 -0400

    json parsing
    added some new son parsing data and son file from London tube system

--- JSONFromFile.playground/Contents.swift
+++ JSONFromFile.playground/Contents.swift
@@ -6,24 +6,72 @@ import Foundation
 //        Toronto that offer an air-conditioned space for temporary relief on heat alert
 //        and extreme heat alert days."
 //
-// SOURCE: http://www1.toronto.ca/wps/portal/contentonly?vgnextoid=e7356d1900531510VgnVCM10000071d60f89RCRD&vgnextchannel=1a66e03bb8d1e310VgnVCM10000071d60f89RCRD
+// SOURCE: https://api.tfl.gov.uk/Line/Route?app_id=&app_key=
 //
 // get the file path for the file "ac-in-toronto" in the playground bundle
-let filePath = NSBundle.mainBundle().pathForResource("ac-in-toronto", ofType: "json")
+let filePath = NSBundle.mainBundle().pathForResource("currency", ofType: "json")
 
 // get the contentData
 let contentData = NSFileManager.defaultManager().contentsAtPath(filePath!)
 
-// get the string (it is an Optional string right now)
-let content = NSString(data: contentData!, encoding: NSUTF8StringEncoding) as? String
+//// get the string (it is an Optional string right now)
+//let content = NSString(data: contentData!, encoding: NSUTF8StringEncoding) as? String
 
 // print the file path
 print("filepath: \(filePath!)")
 
 // This is an optional String, so unwrap it
-if let c = content {
+if let c = contentData {
     print("content: \n\(c)")
-    
+    do {
+        
+        // try to parse entire json file int a single Anyobject
+        let data = try NSJSONSerialization.JSONObjectWithData(c, options: NSJSONReadingOptions.AllowFragments)
+        
+        // cast  Anyopbject into a dictionary with a key of type string and a value that is Any object
+        if let currencyData = data as? [String : AnyObject]{
+            // if this worked, we have dictionary
+            print("====")
+            print("the value for the 'time' key is: ")
+            print(currencyData["bpi"])
+            print("====")
+            
+            // now sort into bpi part of json data and parse further
+            if let exchangeRateDetails = currencyData["bpi"] as? [String :AnyObject] {
+                print("==== US dollars data ====")
+                print(exchangeRateDetails["USD"])
+                print("==== Canadian dollars data ====")
+                print(exchangeRateDetails["CAD"])
+                
+                if let americanCurrencyDetails = exchangeRateDetails as? [String : AnyObject]{
+                    
+                    print("****")
+                    print("stuff")
+                    
+                    // move data into individual variables
+                    gaurd let code : String = americanCurrencyDetails["code"] as? String,
+                    let rate : Double = americanCurrencyDetails["rate"] as? Double {
+                    } else {
+                        print("no worky")
+                    }
+                    
+                } else {
+                    print("error at parsing of JSON at american currency details")
+                }
+            } else {
+                print ("error: could not cast exchange rate details into dictionary")
+            }
+            
+            
+        } else {
+            print("error could not cast json into a dictionary of key type string and value type Anyobject")
+        }
+        
+        // if an error hapens print failed to load
+    } catch let error as NSError {
+     print("Failed to load JSON.")
+        print("\(error.localizedDescription)")
+    }
     // Now try parsing this file (or a file of your choice that you place in the Resources folder
     
     
